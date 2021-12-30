a76e8ceacc34a2fdb33c844c21c9f59016e9a3e2
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2021-12-30 12:36:00 -0300

    Fixing error "Unresolved reference: getRecommended"

--- src/main/java/net/technicpack/launchercore/modpacks/ModpackModel.kt
+++ src/main/java/net/technicpack/launchercore/modpacks/ModpackModel.kt
@@ -323,7 +323,7 @@ class ModpackModel protected constructor() {
     }
 
     fun getRecommendedBuild(): String? {
-        if (packInfo != null && packInfo.getRecommended() != null) return packInfo.getRecommended() else return getBuild()
+        if (packInfo != null && packInfo!!.recommended != null) return packInfo.getRecommended() else return getBuild()
     }
 
     fun getLatestBuild(): String? {
