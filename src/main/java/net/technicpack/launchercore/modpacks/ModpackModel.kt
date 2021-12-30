fee82d41363e2192d2031b70b8e01283cfc2e396
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2021-12-30 12:38:50 -0300

    Fixing error "Unresolved reference: getLatest"

--- src/main/java/net/technicpack/launchercore/modpacks/ModpackModel.kt
+++ src/main/java/net/technicpack/launchercore/modpacks/ModpackModel.kt
@@ -327,7 +327,7 @@ class ModpackModel protected constructor() {
     }
 
     fun getLatestBuild(): String? {
-        if (packInfo != null && packInfo!!.latest != null) return packInfo.getLatest() else return getBuild()
+        if (packInfo != null && packInfo!!.latest != null) return packInfo!!.latest else return getBuild()
     }
 
     fun getWebSite(): String? {
